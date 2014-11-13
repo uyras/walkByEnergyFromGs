97fe91c3313b52d348ac27e201ab4efe12864206
uyras <150030@mail.ru>
2014-11-13 14:43:47 +1000

    Добавлена рекурсивная функция обхода состояний
    Генерируем систему, рекурсивно обходим все состояния, выводим на экран
    среднее значение энергий для набора состояний и среднюю намагниченность
    (с направлениями).

--- main.cpp
+++ main.cpp
@@ -9,51 +9,61 @@
 
 using namespace std;
 
+
+int totalSteps;
+double eAvg, Emin;
+Vect mAvg;
+void walkEnergy(int deepness, PartArray *a, vector<Part*>::iterator iter){
+    if (iter==a->parts.begin()){
+        totalSteps=0;
+        eAvg=0;
+        mAvg = Vect(0,0,0);
+    }
+    if (deepness==0){
+        totalSteps++;
+        mAvg+=a->calcM1();
+        eAvg+=a->calcEnergy1FastIncremental(Emin);
+    }
+
+    Part* temp;
+    int steps=0;
+    while(iter!=a->parts.end()){
+        temp = (*iter);
+        if (!temp->state){
+            temp->rotate();
+            walkEnergy(deepness-1,a,iter);
+            temp->rotate();
+            steps++;
+        }
+        iter++;
+    }
+}
+
 int main(){
     config::Instance()->set2D();
     config::Instance()->srand(time(NULL));
 
-    //PartArray *a = new PartArray(10,10,10,20);
-    //a->save("sys.dat");
-    PartArray *a = new PartArray("sys.dat");
+    PartArray *a = new PartArray(15,15,1,20);
+    //a->save("sys2.dat");
+    //PartArray *a = new PartArray("sys.dat");
     a->setToGroundState();
-    double Emin = a->calcEnergy1();
+    a->state->hardReset();
+    Emin = a->calcEnergy1FastIncrementalFirst();
     cout<<"E="<<Emin<<endl;
     Vect m = a->calcM1();
     cout<<"Mx="<<m.x<<" My="<<m.y<<" |M|="<<m.length()<<endl;
-    vector<Part*>::iterator iter = a->parts.begin(), iter2;
-    Part* temp, *temp2; double Etemp=0; double eTotal = 0;
-    while(iter!=a->parts.end()){
-        temp = (*iter);
-        temp->rotate();
-        Etemp = a->calcEnergy1();
-        eTotal += Etemp;
-        cout<<"Eold="<<Emin<<" Enew="<<Etemp<<" diff="<<Emin-Etemp<<endl;
-        temp->rotate();
-        iter++;
-    }
 
-    cout<<"average E="<<eTotal/(double)a->count()<<endl<<endl;
-
-    eTotal=0;
-    iter=a->parts.begin();
-    while(iter!=a->parts.end()){
-        temp = (*iter);
-        temp->rotate();
-        iter2 = a->parts.begin();
-        while(iter2!=a->parts.end()){
-            temp2 = (*iter2);
-            temp2->rotate();
-            Etemp = a->calcEnergy1();
-            eTotal += Etemp;
-            //cout<<"Eold="<<Emin<<" Enew="<<Etemp<<" diff="<<Emin-Etemp<<endl;
-            temp2->rotate();
-            iter2++;
-        }
-        temp->rotate();
-        iter++;
+    cout<<endl;
+    int stepsSum=0;
+    for (int i=1;i<=a->count();i++){
+        walkEnergy(i,a, a->parts.begin());
+        stepsSum+=totalSteps;
+        cout<<i<<"parts, average E="<<eAvg/(double)totalSteps;
+        cout<<" Mx="<<mAvg.x<<" My="<<mAvg.y<<" |M|="<<mAvg.length()/(double)totalSteps;
+        cout<<" steps="<<totalSteps<<endl;
     }
-    cout<<"average E="<<eTotal/(double)(a->count()*a->count())<<endl<<endl;
+    cout<<endl;
+    cout<<"total steps="<<stepsSum<<" 2^"<<a->count()<<"="<<pow(2,a->count())<<endl;
 
     cout<<"finish";
     return 0;
